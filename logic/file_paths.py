b4072ed9321ad22bb14e50683daa3b8d32feb1e2
kartikbansode <185406501+kartikbansode@users.noreply.github.com>
2026-08-06 17:55:31 +0530

    started to make installer

--- logic/file_paths.py
+++ logic/file_paths.py
@@ -2,10 +2,30 @@ import os
 import sys
 
 
-def data_path(*paths):
+APP_NAME = "QuickBill"
+
+
+def resource_path(*paths):
     if getattr(sys, "frozen", False):
-        base = os.path.dirname(sys.executable)
+        base = sys._MEIPASS
     else:
-        base = os.path.dirname(os.path.abspath(sys.argv[0]))
+        base = os.path.abspath(".")
+
+    return os.path.join(base, *paths)
+
+
+def data_path(*paths):
+    if os.name == "nt":
+        base = os.path.join(
+            os.environ["LOCALAPPDATA"],
+            APP_NAME,
+        )
+    else:
+        base = os.path.join(
+            os.path.expanduser("~"),
+            f".{APP_NAME.lower()}",
+        )
+
+    os.makedirs(base, exist_ok=True)
 
     return os.path.join(base, *paths)
